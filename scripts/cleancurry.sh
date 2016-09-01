78d85d455057950f948b92716fde89f1b0a65d40
Michael Hanus <mh@informatik.uni-kiel.de>
2016-09-01 12:28:39 +0200

    Frontend updated

--- scripts/cleancurry.sh
+++ scripts/cleancurry.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # delete all auxiliary files of all Curry programs in a directory
 
+RM=/bin/rm
+
 RECURSIVE=no
 if [ "xx$1" = "xx-r" ] ; then
   RECURSIVE=yes
@@ -47,52 +49,41 @@ do
   if [ "$F" != "*.curry" -a "$F" != "*.lcurry" ] ; then
     F=`expr $F : '\(.*\)\.lcurry' \| $F`
     F=`expr $F : '\(.*\)\.curry' \| $F`
-    rm -f $F.ast $F.cint $F.fl $F.def $F.pizza $F.acy $F.uacy $F.flc $F.fint $F.fcy $F.pl $F.po $F.pl.main $F.state $F.profile "$F"_flat.xml $F.icurry
-    if [ -h "$F" ] ; then
-      rm "$F"
-    fi
+    $RM -f $F.ast $F.cint $F.fl $F.def $F.pizza $F.flc $F.fint $F.profile "$F"_flat.xml
     FDIR=`dirname $F`
     FBASE=`basename $F`
     PAKCSDIR=$FDIR/.curry/pakcs
     if [ -d $PAKCSDIR ] ; then
-      rm -f $PAKCSDIR/$FBASE.pl $PAKCSDIR/$FBASE.po
+      $RM -f $PAKCSDIR/$FBASE.pl $PAKCSDIR/$FBASE.po
       FDIRFILES=`ls -A $PAKCSDIR`
       if [ -z "$FDIRFILES" ] ; then # pakcs directory is empty
         rmdir $PAKCSDIR
       fi
     fi
-    # and delete .pakcs directory of version 1.9.1
-    OLDPAKCSDIR=$FDIR/.pakcs
-    if [ -d $OLDPAKCSDIR ] ; then
-      rm -rf $OLDPAKCSDIR
-    fi
-    OLDPAKCSDIR=$FDIR/.curry/.pakcs
-    if [ -d $OLDPAKCSDIR ] ; then
-      rm -rf $OLDPAKCSDIR
-    fi
     CURRYDIR=$FDIR/.curry
     if [ -d $CURRYDIR ] ; then
-      rm -f $CURRYDIR/$FBASE.fcy $CURRYDIR/$FBASE.fint $CURRYDIR/$FBASE.icurry $CURRYDIR/$FBASE.acy $CURRYDIR/$FBASE.uacy
+      CURRYF=.curry/$F
+      $RM -f $CURRYF.cy $CURRYF.acy $CURRYF.uacy $CURRYF.fcy $CURRYF.icurry $CURRYF.tokens
       FDIRFILES=`ls -A $CURRYDIR`
       if [ -z "$FDIRFILES" ] ; then # .curry directory is empty
         rmdir $CURRYDIR
       fi
     fi
-    rm -f -r COOSYLOGS
-    rm -f -r $F.classes
+    $RM -f -r COOSYLOGS
+    $RM -f -r $F.classes
   fi
 done
-rm -f prelude.pizza
+$RM -f prelude.pizza
 
 if [ $ALL = yes ] ; then
   for i in *.fcy # look for fcy files not deleted in the first step
   do
     F=`expr $i : '\(.*\).fcy'`
     if [ "$F" != "*" ] ; then
-      rm -f $F.fcy $F.pl $F.po $F.pl.main $F.state $F.profile "$F"_flat.xml
+      $RM -f $F.fcy $F.pl $F.po $F.pl.main $F.state $F.profile "$F"_flat.xml
       FDIR=`dirname $F`
       FBASE=`basename $F`
-      rm -f $FDIR/.curry/pakcs/$FBASE.pl $FDIR/.curry/pakcs/$FBASE.po
+      $RM -f $FDIR/.curry/pakcs/$FBASE.pl $FDIR/.curry/pakcs/$FBASE.po
     fi
   done
 fi
@@ -102,12 +93,14 @@ for F in $LCURRYFILES
 do
   if [ "$F" != "*.lcurry" ] ; then
     F=`expr $F : '\(.*\)\.lcurry' \| $F`
-    rm -f $F.curry
+    $RM -f $F.curry
   fi
 done
 
 if [ $RECURSIVE = yes ]
 then
+  # delete .curry directory:
+  $RM -rf .curry  
   PATHNAME=`(cd "\`dirname \"$0\"\`" > /dev/null ; pwd)`
   for i in *
   do
