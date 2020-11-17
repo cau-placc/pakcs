75f0ff1021dd30374d4c45fe2844fb3987d10e12
Michael Hanus <mh@informatik.uni-kiel.de>
2020-11-17 18:12:57 +0100

    Bug fix: workaround for frontend bug

--- src/readFlcFromFcy.pl
+++ src/readFlcFromFcy.pl
@@ -33,7 +33,9 @@ readFlcFromFcy(FileName,_) :-
 fcy2flcProg('Prog'(ModName,Imps,Types,Funcs,Ops),
 	    'Prog'(FModName,FImps,FTypes,FFuncs,FOps)) :-
         cp_string(ModName,FModName),
-        map2M(basics:cp_string,Imps,FImps),
+        map2M(basics:cp_string,Imps,FImps0),
+        % workaround for frontend bug: import Prelude if it is not imported:
+        (member("Prelude",FImps0) -> FImps = FImps0 ; FImps = ["Prelude"|FImps0]),
 	fcy2flcTypes(Types,FTypes),
 	map2M(readFlcFromFcy:fcy2flcFunc,Funcs,FFuncs),
 	map2M(readFlcFromFcy:fcy2flcOp,Ops,FOps).
