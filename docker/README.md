ba87e04adf34e8eb8fdadde7a0aab97bac224678
Michael Hanus <mh@informatik.uni-kiel.de>
2020-11-19 17:27:20 +0100

    Dockerfiles added

---
+++ docker/README.md
@@ -0,0 +1,60 @@
+Docker image of PAKCS
+=====================
+
+This directory contains some files to create and run the
+[Docker image of PAKCS](https://hub.docker.com/r/caups/pakcs3).
+
+
+Building a new docker image
+---------------------------
+
+If necessary, clean old image:
+
+    > docker image rm pakcs3
+    > docker image prune
+
+Then build new image:
+
+    > docker build -t pakcs3 .
+
+or
+
+    > docker build -t pakcs3 -f Dockerfile-pakcs-... .
+
+
+Uploading image to Docker Hub
+-----------------------------
+
+When the repository does not yet exist on Docker Hub:
+
+1. Log in on https://hub.docker.com
+2. Change to organization "caups"
+3. Click on "Create Repository"
+4. Choose a name ("pakcs3") and click create
+
+When the repository exists on Docker Hub:
+
+Log into the Docker Hub from command line, tag and push the local image:
+
+    > docker login --username mhanus42
+    > docker tag pakcs3 caups/pakcs3:<version>
+    > docker push caups/pakcs3:<version>
+
+where <version> should be something like "3.2.0"
+or "latest" to update the latest version.
+
+
+Running the Docker image of PAKCS
+---------------------------------
+
+For convenient invocation of PAKCS and the tools contained in the
+Docker image, one can use the shell script contained in this directory:
+
+    > ./pakcs3-docker.sh
+
+invokes the interactive REPL of PAKCS. Use
+
+    > ./pakcs3-docker.sh --help
+
+to see all options of this script.
+
---
+++ docker/pakcs3-docker.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# shell script to run the Docker image caups/pakcs3
+# with appropriate options in order to use PAKCS
+# with local files and invoke tools contained in the image
+
+# set docker options:
+# run interactive, remove container after execution
+DOCKEROPTS="-it --rm"
+# mount current working directory and user's home directory
+DOCKEROPTS="$DOCKEROPTS -v `pwd`:`pwd` -w `pwd` -v $HOME:$HOME -e HOME=$HOME"
+# set docker user to host user
+DOCKEROPTS="$DOCKEROPTS -u $(id -u):$(id -g)"
+
+DOCKERTAG="caups/pakcs3"
+ENTRYPOINT=""
+HELP=no
+
+case $1 in
+  --help | -h | -\? ) HELP=yes ;;
+esac
+
+if [ $HELP = yes ] ; then
+  echo "Usage: pakcs3-docker.sh [-h|-?|--help] [-t TAG] [options]"
+  echo ""
+  echo "with options:"
+  echo ""
+  echo "-h|-?|--help       : show this message and quit"
+  echo "-t TAG             : use docker image with tag TAG (default: $DOCKERTAG)"
+  echo "cypm <opts>        : invoke Curry Package Manager with <opts>"
+  echo "curry-check <opts> : invoke CurryCheck with <opts>"
+  echo "curry-doc   <opts> : invoke CurryDoc with <opts>"
+  echo "pakcs <opts>       : invoke PAKCS with <opts>"
+  echo "<opts>             : invoke PAKCS with <opts>"
+  exit
+fi
+
+# check docker image tag:
+if [ $# -gt 1 -a "$1" = "-t" ] ; then
+  shift ; DOCKERTAG=$1 ; shift
+fi
+
+# check whether an installed tool should be invoked:
+case $1 in
+  pakcs       ) shift ;;
+  cypm        ) shift ; ENTRYPOINT="/pakcs/pakcs/bin/cypm" ;;
+  curry-check ) shift ; ENTRYPOINT="/pakcs/cpm/bin/curry-check" ;;
+  curry-doc   ) shift ; ENTRYPOINT="/pakcs/cpm/bin/curry-doc" ;;
+esac
+
+if [ -n "$ENTRYPOINT" ] ; then
+  DOCKEROPTS="$DOCKEROPTS --entrypoint=$ENTRYPOINT"
+fi
+
+docker run $DOCKEROPTS $DOCKERTAG ${1+"$@"}
