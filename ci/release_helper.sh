40f036f78fe9b6315a3165dbf3154a9d8ac517f8
Bennet Bleßmann <bb-github@t-online.de>
2021-07-12 20:37:27 +0200

    fix todo as blocker is resolved
    the blocking issue [Issue 273034](https://gitlab.com/gitlab-org/gitlab/-/issues/273034) is resolved and the used gitlab instance runs a version that has the fix
    
    nightlies and tests are no longer a separate package, as originally intended, as we can now add that information as part of the version
    
    this allows to clean up the packs-nigthly-* and pakcs-test-* packages
    which would also accumulate duplicates of all files for each test/nightly run

--- ci/release_helper.sh
+++ ci/release_helper.sh
@@ -7,28 +7,31 @@
 # takes one parameter indicating whether this is a nightly release or a release release
 function release_helper_init() {
 
-  version="${BUILD_PAKCS_VERSION}"
+  version_number="${BUILD_PAKCS_VERSION}"
   arch="${BUILD_PAKCS_ARCH}"
 
   # FULLNAME and ARCH copied from makefile
   package_name="pakcs"
   name="pakcs-${version}"
 
+  name_suffix=""
+  release_tag=""
+  pre_release=""
+  build=""
+
   case $1 in
   release)
     if [[ ${TEST_RELEASE} == "yes" ]]; then
-      package_suffix="-test-${CI_COMMIT_SHORT_SHA}"
-      name_suffix="-test"
       release_tag="test-${CI_COMMIT_SHORT_SHA}"
+      pre_release="test"
+      build="${CI_COMMIT_SHORT_SHA}"
     elif [[ ${CI_COMMIT_TAG} =~ ^(v|V)[0-9]*\.[0-9]*\.[0-9]*$ ]]; then
-      package_suffix="-release"
-      name_suffix=""
       release_tag="${CI_COMMIT_TAG}"
     elif [[ -n ${CI_COMMIT_TAG} ]]; then
-      # not a version number tag, don't occupy the version for the release package
-      package_suffix="-release-${CI_COMMIT_TAG}"
       name_suffix="-${CI_COMMIT_TAG}"
       release_tag="${CI_COMMIT_TAG}"
+      pre_release="tag"
+      build="${CI_COMMIT_TAG}"
     else
       echo "Release should be triggered by either TEST_RELEASE variable being 'yes'"
       echo "or by git tag, nighter is the case."
@@ -38,23 +41,10 @@ function release_helper_init() {
     fi
     ;;
   nightly)
-    # would like to have the commit hash be part of the version
-    # but gitlab restricts the version to the regex /\A\d+.\d+.\d+.\z/
-    # so we add it as a suffix to the package name and the file name
-    # this sadly means we create a package per nightly
-    # TODO when [Issue 273034](https://gitlab.com/gitlab-org/gitlab/-/issues/273034) is fixed
-    # use a SemVer lable instead of creating a package per nightly
-    # could also drop the package_suffix entirely than releases and nightly would be under the same package
-    #
-    # package_suffix="-nightly"
-    # version="${version}-nightly-${CI_COMMIT_SHORT_SHA}"
-    #
-    # release_tag and name_suffix stay as is
-
-    package_suffix="-nightly-${CI_COMMIT_SHORT_SHA}"
-    name_suffix="-nightly-${CI_COMMIT_SHORT_SHA}"
-
+    name_suffix="-${BUILD_DATE}-${CI_COMMIT_SHORT_SHA}"
     release_tag="nightly-${BUILD_DATE}"
+    pre_release="nightly"
+    build="${BUILD_DATE}.${CI_COMMIT_SHORT_SHA}"
     ;;
   *)
     echo "Expected first parameter have either value 'release' or 'nightly' got '$1'"
@@ -62,12 +52,12 @@ function release_helper_init() {
     ;;
   esac
 
-  full_name="${name}${name_suffix}"
-  full_package_name="${package_name}${package_suffix}"
+  version="${version_number}${pre_release:+-$pre_release}${build:++${build}}"
+  full_name="${name}${pre_release:+-$pre_release}${name_suffix}"
 
-  PACKAGE_REGISTRY_URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/${full_package_name}/${version}"
+  PACKAGE_REGISTRY_URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/${package_name}/${version}"
 
-  # where to finde the file on the runners disk
+  # where to find the file on the runners disk
   LOCAL_FILE_NAMES=(
     "${name}-src.tar.gz"
     "${name}-${arch}.tar.gz"
