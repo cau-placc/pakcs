3dd27a63ec04dda7867b1cfe9555204ce7f64d00
Michael Hanus <mh@informatik.uni-kiel.de>
2025-12-11 22:44:49 +0100

    Adapt pakcs-docker script to new feature for executing saved states inside docker

--- docker/pakcs-docker.sh
+++ docker/pakcs-docker.sh
@@ -15,20 +15,40 @@ DOCKEROPTS="$DOCKEROPTS -v `pwd`:`pwd` -w `pwd` -v $HOME:$HOME -e HOME=$HOME"
 DOCKEROPTS="$DOCKEROPTS -u $(id -u):$(id -g)"
 
 DOCKERTAG="currylang/pakcs"
+DOCKERBIN=""
 ENTRYPOINT=""
 HELP=no
+PSOPT=""             # name of option to process saved executable
+PSCMD=""             # command to process saved executable
 
-case $1 in
-  --help | -h | -\? ) HELP=yes ;;
-esac
+while [ $# -gt 0 -a -z "$ENTRYPOINT$DOCKERBIN" ]; do
+  case $1 in
+    --help | -h | -\? ) shift ; HELP=yes ;;
+    -t                ) shift ; DOCKERTAG=$1 ; shift ;;
+    -x                ) shift ; ENTRYPOINT=$(realpath "$1") ; shift ;;
+    -s                ) shift ; DOCKERBIN=$(realpath "$1") ;
+                        ENTRYPOINT="$DOCKERBIN.bin" ; shift ;;
+    pakcs             ) shift ; ENTRYPOINT="/pakcs/pakcs/bin/pakcs" ;;
+    cypm              ) shift ; ENTRYPOINT="/pakcs/pakcs/bin/cypm" ;;
+    curry-check       ) shift ; ENTRYPOINT="/pakcs/cpm/bin/curry-check" ;;
+    curry-doc         ) shift ; ENTRYPOINT="/pakcs/cpm/bin/curry-doc" ;;
+    *                 ) ENTRYPOINT="/pakcs/pakcs/bin/pakcs" ;;
+  esac
+done
+if [ -z "$ENTRYPOINT" ] ; then
+  ENTRYPOINT="/pakcs/pakcs/bin/pakcs"
+fi
+DOCKEROPTS="$DOCKEROPTS --entrypoint=$ENTRYPOINT"
 
 if [ $HELP = yes ] ; then
-  echo "Usage: pakcs-docker.sh [-h|-?|--help] [-t TAG] [options]"
+  echo "Usage: pakcs-docker.sh [options]"
   echo ""
   echo "with options:"
   echo ""
   echo "-h|-?|--help       : show this message and quit"
   echo "-t TAG             : use docker image with tag TAG (default: $DOCKERTAG)"
+  echo "-x <s> <opts>      : run saved executable <s> inside docker with <opts>"
+  echo "-s <s>             : transform saved executable <s> into docker executable"
   echo "cypm <opts>        : invoke Curry Package Manager with <opts>"
   echo "curry-check <opts> : invoke CurryCheck with <opts>"
   echo "curry-doc   <opts> : invoke CurryDoc with <opts>"
@@ -37,21 +57,23 @@ if [ $HELP = yes ] ; then
   exit
 fi
 
-# check docker image tag:
-if [ $# -gt 1 -a "$1" = "-t" ] ; then
-  shift ; DOCKERTAG=$1 ; shift
+if [ "$ENTRYPOINT" = "/pakcs/pakcs/bin/pakcs" ] ; then
+  PSOPT="--process-state"
+  PSCMD="$(realpath "$0") -t $DOCKERTAG -s"
 fi
 
-# check whether an installed tool should be invoked:
-case $1 in
-  pakcs       ) shift ;;
-  cypm        ) shift ; ENTRYPOINT="/pakcs/pakcs/bin/cypm" ;;
-  curry-check ) shift ; ENTRYPOINT="/pakcs/cpm/bin/curry-check" ;;
-  curry-doc   ) shift ; ENTRYPOINT="/pakcs/cpm/bin/curry-doc" ;;
-esac
-
-if [ -n "$ENTRYPOINT" ] ; then
-  DOCKEROPTS="$DOCKEROPTS --entrypoint=$ENTRYPOINT"
+if [ -n "$DOCKERBIN" ] ; then
+  if [ $# -gt 0 ] ; then
+    echo "ERROR: Superfluous options: $*"
+    exit 1
+  fi
+  /bin/mv "$DOCKERBIN" "$DOCKERBIN.bin"
+  echo "Original executable moved to '$DOCKERBIN.bin'"
+  echo "#!/bin/sh" > "$DOCKERBIN"
+  echo "docker run $DOCKEROPTS $DOCKERTAG \${1+\"\$@\"}" >> "$DOCKERBIN"
+  chmod 755 "$DOCKERBIN"
+  echo "New docker execution script written to '$DOCKERBIN'"
+else
+  echo docker run $DOCKEROPTS $DOCKERTAG $PSOPT "$PSCMD" ${1+"$@"}
+  docker run $DOCKEROPTS $DOCKERTAG $PSOPT "$PSCMD" ${1+"$@"}
 fi
-
-docker run $DOCKEROPTS $DOCKERTAG ${1+"$@"}
